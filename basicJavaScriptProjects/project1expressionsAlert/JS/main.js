3d20f8f5cea214e7af513a43b384e095a3036bb5
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-21 20:01:42 -0800

    Update main.js

--- basicJavaScriptProjects/project1expressionsAlert/JS/main.js
+++ basicJavaScriptProjects/project1expressionsAlert/JS/main.js
@@ -1,28 +1,30 @@
-alert("Hello, World!");
+alert("Hello, World!");     /**Makes popup window */
 
-window.alert("Hello, World!");
+window.alert("Hello, World!");  /**also makes popup window */
 
-document.write("Hello, World!");
+document.write("Hello, World!");    /**prints on the page */
 
-var a="This is a string";
+var a="This is a string";           /**Makes a variable */
 
 document.write(a);
 
-var s="\"Hello!\"";
+var s="\"Hello!\"";             /**\ Makes " visible in text */
 
 window.alert(s);
 
-document.write("Will you work?"+"Why indeed!");
+document.write("Will you work?"+"Why indeed!"); /**add strings */
 
 var w="why would you"+"add strings??";
 
 document.write(w);
 
-var amber="amber",
+var amber="amber",                          /**declare multiple variables */
     billy="billy";
 
 document.write(amber, billy);
 
 var red="red";
-var red=red.fontcolor(red);
+var red=red.fontcolor(red);                 /**change font color */
 document.write(red);
+
+document.write(3+3);
