f1f2cc7ebe63741f41c29932551c34370048135b
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-23 13:10:30 -0800

    re-comment and <aside>

--- basicJavaScriptProjects/project1expressionsAlert/JS/main.js
+++ basicJavaScriptProjects/project1expressionsAlert/JS/main.js
@@ -1,30 +1,30 @@
-alert("Hello, World!");     /**Makes popup window */
+alert("Hello, World!");     //Makes popup window
 
-window.alert("Hello, World!");  /**also makes popup window */
+window.alert("Hello, World!");  //also makes popup window
 
-document.write("Hello, World!");    /**prints on the page */
+document.write("Hello, World!");    //prints on the page
 
-var a="This is a string";           /**Makes a variable */
+var a="This is a string";           //Makes a variable
 
 document.write(a);
 
-var s="\"Hello!\"";             /**\ Makes " visible in text */
+var s="\"Hello!\"";             //\ Makes " visible in text
 
 window.alert(s);
 
-document.write("Will you work?"+"Why indeed!"); /**add strings */
+document.write("Will you work?"+"Why indeed!"); //add strings
 
 var w="why would you"+"add strings??";
 
 document.write(w);
 
-var amber="amber",                          /**declare multiple variables */
+var amber="amber",                          //declare multiple variables
     billy="billy";
 
 document.write(amber, billy);
 
 var red="red";
-var red=red.fontcolor(red);                 /**change font color */
+var red=red.fontcolor(red);                 //change font color
 document.write(red);
 
 document.write(3+3);
