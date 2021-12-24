5c9e87601488333a20dd8f45aefe0baaa7014446
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-24 10:37:22 -0800

    comments

--- basicJavaScriptProjects/project3MathOperators/JS/main.js
+++ basicJavaScriptProjects/project3MathOperators/JS/main.js
@@ -1,5 +1,5 @@
 function add() {
-    var answer = 5+9;
+    var answer = 5+9;       //mostly self explanitory. all of these do math
     document.getElementById("add").innerHTML = answer;
 }
 
@@ -18,31 +18,33 @@ function divide() {
     document.getElementById("divide").innerHTML = answer;
 }
 
-function manyMaths() {
+function manyMaths() {      //this does a few math equations at the same time
     var answer = 5*9*(2 + 3) - 10;
     document.getElementById("simpleMath").innerHTML = answer;
 }
 
 function remain() {
-    var answer = 9%5;
+    var answer = 9%5;       //find the remainder
     document.getElementById("remainder").innerHTML = answer;
 }
 
 function negitive() {
-    var answer = 13;
+    var answer = 13;                        //this \|/ adds a "-" in front making it negative
     document.getElementById("minus").innerHTML = -answer;
 }
 
 function plus() {
     var x = 10;
-    x++;
+    x++;            //incremeant
     document.getElementById("increase").innerHTML = x
 }
 
 function minus() {
     var x = 10;
-    x--;
+    x--;        //decremeant
     document.getElementById("decrease").innerHTML = x
 }
 
 window.alert(Math.round(Math.random()*50))
+//makes a popup window that has a random number between 1-50
+//Math.round just rounds so you don't get any strange numbers
