e972256d993202867293af111f87e2484355aa45
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-24 09:24:54 -0800

    Added basic math functions

---
+++ basicJavaScriptProjects/project3MathOperators/JS/main.js
@@ -0,0 +1,19 @@
+function add() {
+    var answer = 5+9;
+    document.getElementById("add").innerHTML = answer;
+}
+
+function subtract() {
+    var answer = 5-9;
+    document.getElementById("subtract").innerHTML = answer;
+}
+
+function multiply() {
+    var answer = 5*9;
+    document.getElementById("multiply").innerHTML = answer;
+}
+
+function divide() {
+    var answer = 5/9;
+    document.getElementById("divide").innerHTML = answer;
+}
