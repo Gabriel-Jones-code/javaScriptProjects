ed2b91e655d3c4b20c1aed9531b095c56440a6cb
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-26 11:38:50 -0800

    Update
    Changed .innerHTML = gabe to gabe.age due to an error. I also am starting to write more notes to make the code easy to read

--- basicJavaScriptProjects/project4Dictionary/JS/main.js
+++ basicJavaScriptProjects/project4Dictionary/JS/main.js
@@ -1,10 +1,15 @@
 function dictionary () {
-    var gabe = {
-        age: 20,
+    var gabe = {            //this is a KVP
+        age: 20,            //numbers don't need ""
         eyeColor: "Brown",
         hairColor: "Brown",
-        gender: "Non-Binary",
-        language: "English, Mandarin"
+        gender: "Non-Binary",       //yes... I'm Non-binary
+        language: "English, Mandarin"   //the , doesn't really seperate it.
+                                        //It just makes the text a little better
     };
-    document.getElementById("gabe").innerHTML;
+    document.getElementById("dictionary").innerHTML = gabe.age;
+    //In the html file, I HAD to use dictionary as my id. both for my sake
+    //and for whoever might read this. If you just return the variable "gabe",
+    //You'll get an error because JS reconizes the var and an object.
+    //You need to use .KVP to specify the output
 }
