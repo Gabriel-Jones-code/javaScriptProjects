4e382a5b53374f0d6fe9061eb573ff7f589b0a2c
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-26 11:26:16 -0800

    Added Dictionary

---
+++ basicJavaScriptProjects/project4Dictionary/JS/main.js
@@ -0,0 +1,10 @@
+function dictionary () {
+    var gabe = {
+        age: 20,
+        eyeColor: "Brown",
+        hairColor: "Brown",
+        gender: "Non-Binary",
+        language: "English, Mandarin"
+    };
+    document.getElementById("gabe").innerHTML;
+}
