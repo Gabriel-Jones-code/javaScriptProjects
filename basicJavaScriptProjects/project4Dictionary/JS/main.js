fd7ec19653bc425e21c78ce505520a93d78b8c37
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-26 12:26:07 -0800

    Update main.js

--- basicJavaScriptProjects/project4Dictionary/JS/main.js
+++ basicJavaScriptProjects/project4Dictionary/JS/main.js
@@ -8,7 +8,9 @@ function dictionary () {
         language: "English, Mandarin"   //the , doesn't really seperate it.
                                         //It just makes the text a little better
     };
+    delete gabe.gender;
     document.getElementById("dictionary").innerHTML = gabe.age;
+    document.getElementById("dictionary").innerHTML = gabe.gender;
     //In the html file, I HAD to use dictionary as my id. both for my sake
     //and for whoever might read this. If you just return the variable "gabe",
     //You'll get an error because JS reconizes the var and an object.
