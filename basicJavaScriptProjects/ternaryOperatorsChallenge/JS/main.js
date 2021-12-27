fcd785e6de2ab9ca3d86a60c17006b309a896a89
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-26 18:38:02 -0800

    Please test this
    I named the variable "old" and made the function "age" in JS

---
+++ basicJavaScriptProjects/ternaryOperatorsChallenge/JS/main.js
@@ -0,0 +1,6 @@
+function age() {
+    var age, howOld;
+    age = document.getElementById(age).value    //takes value from html
+    howOld = (age < 18) ? "You are not old enough" : "You are old enough";
+    document.getElementById("old").innerHTML = howOld + " to vote";
+}
