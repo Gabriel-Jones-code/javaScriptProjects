6b9f779458d0c1684b27e7152250a69e1e37469c
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-26 17:17:31 -0800

    A ton of Boolean that I should have pushed

--- basicJavaScriptProjects/project5ComparisonsTypeCoercion/JS/main.js
+++ basicJavaScriptProjects/project5ComparisonsTypeCoercion/JS/main.js
@@ -16,4 +16,27 @@ document.write(-2E310);     //same thing as /|\ but negative
 //BOOLEAN!!
 document.write(23 > 20);                //will return true
 document.write("four" > "three");       //counts letters
-document.write([6 > 4] || [8 < 12]);    //|| is AND. I just remember this from a little bit of collage.
+document.write([6 > 4] || [8 < 12]);    //|| is OR. I just remember this from a little bit of collage.
+
+console.log(2 + 2);       //gives output in console
+
+console.log(6 > 9);
+
+document.write(10 == 10);
+document.write(9 == 10);
+
+var x = 2;
+var y = 2;
+var z = 8;
+
+document.write(x === y);        //=== is comparing var
+document.write(x === z);
+
+document.write([6 < 4] && [4 < 6]);     //not makes everything send the opposite result
+ function not() {
+     document.getElementById("not").innerHTML = !(5 > 10);
+ }
+
+ function not1() {
+    document.getElementById("not1").innerHTML = !(10 > 5);
+}
