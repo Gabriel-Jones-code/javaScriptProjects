c6921b74bc255ceaebb84520bb2fad05b34daa43
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-28 15:04:37 -0800

    comments

--- basicJavaScriptProjects/project8stringMethods/JS/Main.js
+++ basicJavaScriptProjects/project8stringMethods/JS/Main.js
@@ -12,15 +12,19 @@ function cut() {
     var part = sentence.slice(15, 22);
     document.getElementById("slice").innerHTML = part;
 }
+//This lets me cut out part of a string by defining the start and end position
+//of the string I want to keep,
 
 function upper() {
     var hello = "Hello, World!";
     var upper = hello.toUpperCase();
     document.getElementById("upper").innerHTML = upper;
 }
+//This lets me uppercase an entire string
 
 function look() {
     var string = "The boy is looking at a tree";
     var looking = string.search("tree");
     document.getElementById("find").innerHTML = looking;
 }
+//this will tell me the position of a letter or word in a string
