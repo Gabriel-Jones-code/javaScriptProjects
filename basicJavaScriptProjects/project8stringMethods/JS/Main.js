e444611d8c1eafdb24c870572b3216640be9b398
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-28 14:39:24 -0800

    bugged function

--- basicJavaScriptProjects/project8stringMethods/JS/Main.js
+++ basicJavaScriptProjects/project8stringMethods/JS/Main.js
@@ -4,5 +4,11 @@ function sentence(){
     var part3 = "a ";
     var part4 = "sentence";
     document.getElementById("sent").innerHTML =  part1.concat(part2, part3, part4);
-    
+
+}
+
+function cut() {
+    var sentence = "I like to play Pokemon";
+    var part = sentence.slice(15, 21);
+    document.getElementById("slice").innerHTML = section;
 }
