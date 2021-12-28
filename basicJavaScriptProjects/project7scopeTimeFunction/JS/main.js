7c97dc4614da16adc960f3e7d98d96546cb904fd
Gabe <GABEHJ2001@GMAIL.COM>
2021-12-28 11:32:07 -0800

    COMMENTS!!

--- basicJavaScriptProjects/project7scopeTimeFunction/JS/main.js
+++ basicJavaScriptProjects/project7scopeTimeFunction/JS/main.js
@@ -17,13 +17,17 @@ function greeting() {
     if (new Date().getHours() < 12) {
         document.getElementById("time").innerHTML = "Good Morning!";
     }
+    //if the hour is less then 12pm, say Good Morning!
     else if (new Date().getHours() < 15) {
         document.getElementById("time").innerHTML = "Good Afternoon!";
     }
+    //if the hour is greater then 12, but less then 15, say Good Afternoon!
     else if (new Date().getHours() < 18) {
         document.getElementById("time").innerHTML = "Good Evening!";
     }
+    //if the hour is greater then 15, but less then 18, say Good Evening!
     else {
         document.getElementById("time").innerHTML = "Hello!"
     }
+    //if all else fails or the time is outside of the time range, say Hello!
 }
