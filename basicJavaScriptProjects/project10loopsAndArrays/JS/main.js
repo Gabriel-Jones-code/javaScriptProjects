428cea12099f385abac11224add0bca95403df02
Gabe <GABEHJ2001@GMAIL.COM>
2022-01-01 13:15:51 -0800

    learned how to use length

--- basicJavaScriptProjects/project10loopsAndArrays/JS/main.js
+++ basicJavaScriptProjects/project10loopsAndArrays/JS/main.js
@@ -1,9 +1,16 @@
 function callLoop() {
-    var x = 0;
-    var number = "";
-    while(x < 21){
+    var x = 0;                          //number
+    var number = "";                    //string
+    while(x < 21){                      //stop when x hits 21
         number += "<br>" + x;           //addes <br> + x to number
-        x++;
+        x++;                            //x + 1
     }
     document.getElementById("loop").innerHTML = number;
 }
+
+const friends = ["Celia", " Kaylee", " Emily", " Tyler"];
+let numberFriends = friends.length;
+friends.length = 4;
+
+document.getElementById("friends").innerHTML = friends;
+document.getElementById("length").innerHTML = numberFriends;
