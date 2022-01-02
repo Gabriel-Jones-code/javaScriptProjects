8723eb5bfc932ec25a5a2e91ef433ed340c428a6
Gabe <GABEHJ2001@GMAIL.COM>
2022-01-01 22:36:19 -0800

    comments and code

--- basicJavaScriptProjects/project10loopsAndArrays/JS/main.js
+++ basicJavaScriptProjects/project10loopsAndArrays/JS/main.js
@@ -17,3 +17,13 @@ friends.length = 4;                             //will display array from 1 - 4
 document.getElementById("words").innerHTML = string;
 document.getElementById("friends").innerHTML = friends;
 document.getElementById("length").innerHTML = numberFriends;
+
+
+function forLoop() {
+    var dog = ["Max", "Hemi", "George", "Rockey"];
+    var content = "";                       //leave empty to add words into var
+    for(let n = 0; n < dog.length; n++){    //declared let inside of for loop
+        content += dog[n] + "<br>";     //adds dog[n] to content
+    }                                   //content will hold everthing at once
+    document.getElementById("for").innerHTML = content;
+}
