1ad1aef117c6c28c7e8299e407675720700de47f
Gabe <GABEHJ2001@GMAIL.COM>
2022-01-04 22:59:19 -0800

    Part 1 of JS done

---
+++ JavaScriptProject/JavaScript/JavaScript.js
@@ -0,0 +1,16 @@
+//use a switch statement
+
+function pet() {
+    var output = "";            //used to hold a output to send to html
+    var animals = document.getElementById("answer").value;       //gets value from html
+    var string = "s make great pets!";    //use for end of string. Not "REQUIRED", but reduces repitition
+    switch(animals) {       //a conditional statement based on the cases inside
+        case "Dog":                             //case...var: code
+            output = animals + string;
+            break;
+        default:
+            output = "Please enter an animal \"Exactly\" as shown"
+            break;
+    }
+    document.getElementById("pets").innerHTML = output;
+}
