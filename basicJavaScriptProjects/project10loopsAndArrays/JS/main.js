1d5fa7f654aa9c14bc498167159dbad85391c945
Gabe <GABEHJ2001@GMAIL.COM>
2022-01-04 20:44:07 -0800

    I should have uploaded a while ago

--- basicJavaScriptProjects/project10loopsAndArrays/JS/main.js
+++ basicJavaScriptProjects/project10loopsAndArrays/JS/main.js
@@ -8,7 +8,9 @@ function callLoop() {
     document.getElementById("loop").innerHTML = number;
 }
 
-let string = "this is a sentance";
+let string = "this is a sentance";              //let is for a block
+//let in this case only applies to anything outside of a block
+//if used in any other context, let cannot be used outside of it's block
 string = string.length;     //counts letters
 const friends = ["Celia", " Kaylee", " Emily", " Tyler"];   //array that will not change
 let numberFriends = friends.length;             //counts items in array
@@ -35,4 +37,41 @@ function arrayFunction() {
     array[2] = "Konichiwa!";
     array[3] = "Ni Hao!";        
     document.getElementById("array").innerHTML = array[3];
+}
+
+function constant() {
+    const computer = {gpu:"1080", cpu:"ryzen7", memory:"32gb"};     //const is not expected to change
+    computer.gpu = "3070";                                          //this changes an element of computer
+    document.getElementById("constant").innerHTML = computer.gpu;
+}
+
+function returns() {
+    let x = 0;
+    x += 50;
+    return x;
+}
+
+document.getElementById("refund").innerHTML = returns();
+
+function object() {
+    let classroom = {
+        students: 30,
+        subject: "Math",
+        period: 4,
+    }
+document.getElementById("object").innerHTML = classroom.students + " students are in the "
+                                            + classroom.subject + " class. This is period "
+                                            + classroom.period;
+}
+
+function bnc() {
+    for (let i = 0; i < 7; i++) {
+        if (i === 3) {break;}       //this stops the loop
+        document.getElementById("break").innerHTML = i;
+    }
+
+    for (let i = 0; i < 7; i++) {
+        if (i === 5) {continue;}    //skips one portion of the code
+        document.getElementById("continue").innerHTML = i;
+    }
 }
