fe4447be9a6de791178fd73a74c41f33275a4abb
Gabe <GABEHJ2001@GMAIL.COM>
2022-01-05 18:45:48 -0800

    Update JavaScript2.js

--- JavaScriptProject/JavaScript/JavaScript2.js
+++ JavaScriptProject/JavaScript/JavaScript2.js
@@ -1,7 +1,7 @@
 function validate() {
-    let x = document.forms["form1"]["firstName"].value;
+    let x = document.forms["form1"]["firstName"].value; //grabs id from form
     let y = document.forms["form1"]["lastName"].value;
-    if (x == "" || y == "") {
+    if (x == "" || y == "") {                           // || is the OR operator
         alert("Please enter in the following information");
         return false;
     }
